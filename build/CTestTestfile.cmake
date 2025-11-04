3097f6a3e4301b86406597b4b73300a356192f7b
Rahul Amudhasagaran <ramudhas@ucsc.edu>
2025-11-03 20:53:08 -0800

    removed build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ramudhas/cmpm123/connect-4-starter
-# Build directory: /home/ramudhas/cmpm123/connect-4-starter/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
